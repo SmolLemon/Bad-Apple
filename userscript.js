e28d50e6ee64d6e2356cb2197d125c792787365d
Smol Lemon <154004111+SmolLemon@users.noreply.github.com>
2023-12-26 12:36:47 +0000

    Add comments on userscript.js

--- userscript.js
+++ userscript.js
@@ -1,6 +1,5 @@
-//fetch('./data.json')
 function start_bad_apple(){
-    fetch('http://127.0.0.1:5500/data.json')
+    fetch('http://127.0.0.1:5500/data.json') 
     .then((response) => response.json())
     .then((json) => {
         let frame = json.frame;
@@ -13,7 +12,7 @@ function start_bad_apple(){
                 for (let i=0; i<rows.length; ++i) {
                 if(i === 1 || i == 3 || i === 5) continue;
                 let item = rows[i].getElementsByTagName("td")[1];
-                item.removeChild(item.firstElementChild);
+                item.removeChild(item.firstElementChild); //remove the "Accepted" text
                 
                 for (let j=0; j<33; ++j) {
                     let a = document.createElement("span");
@@ -25,19 +24,20 @@ function start_bad_apple(){
             }
         }, 15000);
 
+        
         async function print(fr){
             let frames = frame[fr];
             for (let k=0, i = 0; k<rows.length; k++) {
                 if(k === 1 || k == 3 || k === 5) continue;
                 let item = rows[k].getElementsByTagName("td")[1];
                 let pixels = item.getElementsByTagName("span");
-                for (let j=0; j<frames[i].length; j++) {      
+                for (let j=0; j<frames[i].length; j++) {
+                    // AC for white pixels, WA for black pixels
                     if (frames[i][j] === 1) {
                         pixels[j].textContent = "AC";
                         pixels[j].setAttribute("title", "Accepted");
                         pixels[j].setAttribute("class", "case-AC mono");  
-                    }
-                    else {
+                    } else {
                         pixels[j].textContent = "WA";
                         pixels[j].setAttribute("title", "Wrong Answer");
                         pixels[j].setAttribute("class", "case-WA mono");  
@@ -50,6 +50,9 @@ function start_bad_apple(){
             return new Promise(resolve => setTimeout(resolve, ms));
         }
         async function run(fr) {
+            //The bad apple video runs in 30 frames per second.
+            //I tried to run 30 frames but it was too laggy to put on video
+            //So I slowed it down to 10 frames per second
             await sleep(fr * 100 + 30000);
             print(fr);
         }
