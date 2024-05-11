0fb904ed81b0bce7c11d417c9876b64f45fc2917
Smol Lemon <154004111+SmolLemon@users.noreply.github.com>
2024-05-11 08:44:06 +0700

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -5,13 +5,11 @@ function start_bad_apple(){
         let frame = json.frame;
         let dStyle = document.querySelector('head');
         dStyle.innerHTML += '<style> .mono {font-family: monospace;} </style>';
-
-
         let rows = document.getElementsByTagName("tr");
         setTimeout(() => {
                 for (let i = 0; i < rows.length; ++i) {
+                if(rows[i].id.slice(-1) === "t") continue;
                 let item = rows[i].getElementsByTagName("td")[1];
-                if(item.id.slice(-1) === "t") continue;
                 item.removeChild(item.firstElementChild); //remove the "Accepted" text
                 
                 for (let j = 0; j < 33; ++j) {
@@ -28,8 +26,8 @@ function start_bad_apple(){
         async function makeApple(fr){
             let frames = frame[fr];
             for (let k = 0, i = 0; k < rows.length; ++k) {
+                if(rows[k].id.slice(-1) === "t") continue;
                 let item = rows[k].getElementsByTagName("td")[1];
-                if(item.id.slice(-1) === "t") continue;
                 let pixels = item.getElementsByTagName("span");
                 for (let j = 0; j < frames[i].length; ++j) {
                     // AC for white pixels, WA for black pixels
@@ -53,7 +51,7 @@ function start_bad_apple(){
             //The bad apple video runs in 30 frames per second.
             //I tried to run 30 frames but it was too laggy to put on video
             //So I slowed it down to 10 frames per second
-            await sleep(fr * 100 + 10000);
+            await sleep(fr * 100 + 8000);
             makeApple(fr);
             if(fr === 0) console.log("Start Bad Apple on VNOJ by SmolLemon");
             if((fr + 1) % 100 === 0) console.log(fr + 1);
